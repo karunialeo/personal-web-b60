88ef2bb22afe8343feffd5f90c5df9d311c9cad8
Karunia Leo Gultom <karunialeo@gmail.com>
2025-02-19 11:06:54 +0700

    authentication and authorization setup done

--- controllers/controller-v2.js
+++ controllers/controller-v2.js
@@ -1,19 +1,161 @@
 const { Sequelize } = require("sequelize");
+const bcrypt = require("bcrypt");
 const config = require("../config/config.json");
-const { Blog } = require("../models");
+const { Blog, User } = require("../models");
 
 const sequelize = new Sequelize(config.development);
 
+const saltRounds = 10;
+
+async function renderHome(req, res) {
+  const user = req.session.user;
+  console.log("usernya adalah :", user);
+  res.render("index", { user: user });
+}
+
+async function renderContact(req, res) {
+  const user = req.session.user;
+  console.log("usernya adalah :", user);
+
+  res.render("contact", { user: user });
+}
+
+async function renderTestimonials(req, res) {
+  const user = req.session.user;
+  console.log("usernya adalah :", user);
+
+  res.render("testimonials", { user: user });
+}
+
+async function renderLogin(req, res) {
+  const user = req.session.user;
+  console.log("usernya adalah :", user);
+
+  if (user) {
+    req.flash("warning", "User already login.");
+    res.redirect("/");
+  } else {
+    res.render("auth-login", { user: user });
+  }
+}
+
+async function renderRegister(req, res) {
+  const user = req.session.user;
+  console.log("usernya adalah :", user);
+
+  if (user) {
+    res.redirect("/");
+  } else {
+    res.render("auth-register", { user: user });
+  }
+}
+
+async function authLogin(req, res) {
+  const { email, password } = req.body;
+  // check kalau usernya ada atau tidak
+  const user = await User.findOne({
+    where: {
+      email: email,
+    },
+  });
+
+  if (!user) {
+    req.flash("error", "User tidak ditemukan.");
+    return res.redirect("/login");
+  }
+
+  // check kalau passwordnya salah
+  const isValidated = await bcrypt.compare(password, user.password); // return sebuah boolean, apakah true atau false
+
+  if (!isValidated) {
+    req.flash("error", "Password mismatch.");
+    return res.redirect("/login");
+  }
+
+  let loggedInUser = user.toJSON(); // convert dari object sequelize ke object biasa
+
+  delete loggedInUser.password; // menghapus properti password pada object new user
+
+  console.log("user setelah passwordnya di delete :", loggedInUser);
+  req.session.user = loggedInUser;
+
+  req.flash("success", `Selamat datang, ${loggedInUser.name}!`);
+  res.redirect("/");
+}
+
+async function authRegister(req, res) {
+  const { name, email, password, confirmPassword } = req.body; // object destructuring
+
+  if (password != confirmPassword) {
+    req.flash("error", "Password dan confirm password tidak sesuai.");
+    return res.redirect("/register");
+  }
+
+  // check apakah email sudah terpakai
+  const user = await User.findOne({
+    where: {
+      email: email,
+    },
+  });
+
+  if (user) {
+    console.log("usernya udah ada");
+    req.flash("error", "Email sudah terpakai.");
+    return res.redirect("/register");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  const newUser = {
+    name: name,
+    email: email,
+    password: hashedPassword,
+  };
+
+  console.log("user baru :", newUser);
+
+  const userInsert = await User.create(newUser);
+
+  req.flash("success", "Berhasil mendaftar. Silahkan login.");
+  res.redirect("/login");
+}
+
+async function authLogout(req, res) {
+  // hapus user dari session
+  req.session.user = null;
+
+  res.redirect("/login");
+}
+
 async function renderBlog(req, res) {
+  const user = req.session.user;
+  console.log("usernya adalah :", user);
+
+  // kalau usernya ada atau kalau ada yang sedang login
   const blogs = await Blog.findAll({
+    include: {
+      model: User,
+      as: "user",
+      attributes: { exclude: ["password"] },
+    },
     order: [["createdAt", "DESC"]],
   });
 
   console.log("hasil fetch data dari controller v2", blogs);
-  res.render("blog-list", { blogs: blogs });
+
+  // console.log("pemilik blog paling atas :", blogs[0].user);
+
+  if (user) {
+    res.render("blog-list", { blogs: blogs, user: user });
+  } else {
+    res.render("blog-list", { blogs: blogs });
+  }
 }
 
 async function renderBlogDetail(req, res) {
+  const user = req.session.user;
+  console.log("usernya adalah :", user);
+
   const id = req.params.id;
 
   // type data nya adalah object bukan array
@@ -28,13 +170,20 @@ async function renderBlogDetail(req, res) {
   } else {
     console.log("v2 blog detail :", blogYangDipilih);
 
-    res.render("blog-detail", { blog: blogYangDipilih }); // tidak perlu index
+    res.render("blog-detail", { blog: blogYangDipilih, user: user }); // tidak perlu index
   }
 }
 
 async function renderBlogCreate(req, res) {
-  // render halaman create blog
-  res.render("blog-create");
+  // memunculkan halaman create blog
+  const user = req.session.user;
+  console.log("usernya adalah :", user);
+
+  if (user) {
+    res.render("blog-create");
+  } else {
+    res.redirect("/login");
+  }
 }
 
 async function createBlog(req, res) {
@@ -72,6 +221,9 @@ async function deleteBlog(req, res) {
 }
 
 async function renderBlogEdit(req, res) {
+  const user = req.session.user;
+  console.log("usernya adalah :", user);
+
   // render halaman edit blog
   const id = req.params.id;
 
@@ -81,12 +233,16 @@ async function renderBlogEdit(req, res) {
     },
   });
 
+  if (!user) {
+    return res.redirect("/login");
+  }
+
   if (blogYangDipilih === null) {
     res.render("page-404");
   } else {
     console.log("v2 blog detail :", blogYangDipilih);
 
-    res.render("blog-edit", { blog: blogYangDipilih });
+    res.render("blog-edit", { blog: blogYangDipilih, user: user });
   }
 }
 
@@ -117,7 +273,22 @@ async function updateBlog(req, res) {
   res.redirect("/blog");
 }
 
+async function renderError(req, res) {
+  const user = req.session.user;
+  console.log("usernya adalah :", user);
+
+  res.render("page-404", { user: user });
+}
+
 module.exports = {
+  renderHome,
+  renderContact,
+  renderTestimonials,
+  renderLogin,
+  renderRegister,
+  authLogin,
+  authRegister,
+  authLogout,
   renderBlog,
   renderBlogDetail,
   deleteBlog,
@@ -125,4 +296,5 @@ module.exports = {
   createBlog,
   renderBlogEdit,
   updateBlog,
+  renderError,
 };
--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const app = express();
 const hbs = require("hbs");
 const path = require("path");
 const methodOverride = require("method-override");
+const flash = require("express-flash");
+const session = require("express-session");
 
 // const { renderBlogEdit, updateBlog } = require("./controllers/controller-v1");
 
 const {
+  renderHome,
+  renderLogin,
+  renderRegister,
+  renderContact,
+  renderTestimonials,
+  authLogin,
+  authRegister,
+  authLogout,
   renderBlog,
   renderBlogDetail,
   deleteBlog,
@@ -14,6 +24,7 @@ const {
   createBlog,
   renderBlogEdit,
   updateBlog,
+  renderError,
 } = require("./controllers/controller-v2");
 
 const { formatDateToWIB, getRelativeTime } = require("./utils/time");
@@ -23,10 +34,20 @@ const port = 3000;
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "./views")); // C://My Computer/Users/Documents/../../personal-web/views
 
+// modul apa saja yang kita gunakan di dalam express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("assets"));
 app.use(methodOverride("_method"));
+app.use(flash());
+app.use(
+  session({
+    name: "my-session",
+    secret: "askdj3ajwklfj3k3454",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 hbs.registerPartials(__dirname + "/views/partials", function (err) {});
 hbs.registerHelper("equal", function (a, b) {
@@ -36,14 +57,20 @@ hbs.registerHelper("formatDateToWIB", formatDateToWIB);
 hbs.registerHelper("getRelativeTime", getRelativeTime);
 
 // HOME
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.get("/", renderHome);
+
+app.get("/login", renderLogin);
+
+app.get("/register", renderRegister);
+
+app.get("/logout", authLogout);
+
+app.post("/login", authLogin);
+
+app.post("/register", authRegister);
 
 // CONTACT ME
-app.get("/contact", (req, res) => {
-  res.render("contact");
-});
+app.get("/contact", renderContact);
 
 // BLOG LIST
 app.get("/blog", renderBlog);
@@ -67,13 +94,9 @@ app.delete("/blog/:id", deleteBlog);
 app.get("/blog/:id", renderBlogDetail);
 
 // TESTIMONIALS
-app.get("/testimonials", (req, res) => {
-  res.render("testimonials");
-});
+app.get("/testimonials", renderTestimonials);
 
-app.get("*", (req, res) => {
-  res.render("page-404");
-});
+app.get("*", renderError);
 
 app.listen(port, () => {
   console.log(`My personal web app listening on port ${port}`);
