b42dbd2b74ecb7314c388a4548b49ae9e46c80e9
Karunia Leo Gultom <karunialeo@gmail.com>
2025-02-20 12:11:22 +0700

    fix postgres port

--- config/config.js
+++ config/config.js
@@ -7,6 +7,7 @@ module.exports = {
     password: "13572468",
     database: "b60_personalweb",
     host: "127.0.0.1",
+    port: 5432,
     dialect: "postgres",
     dialectModule: pg,
   },
@@ -15,6 +16,7 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
     dialect: "postgres",
     dialectModule: pg,
     dialectOptions: {
